442a5591c8487f38368d0e2d5053c5594a7cf95f
Felippe Vieira <felippe.vieira@stgen.com>
2025-03-21 08:37:03 -0300

    Add project files.

---
+++ Movies.Application/ApplicationServicesExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Repositories;
+
+namespace Movies.Application
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IMovieRepository, MovieRepository>();
+            return services;
+        }
+    }
+}
