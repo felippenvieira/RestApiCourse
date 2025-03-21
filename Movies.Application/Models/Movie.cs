3ab2f7016ed5c601e13ab03ee88f53e2b7d14184
Felippe Vieira <felippe.vieira@stgen.com>
2025-03-21 08:50:51 -0300

    Enhance movie retrieval with slug support
    Updated API endpoints and controller to allow movie retrieval by slug or ID. Added Slug property to Movie and MovieResponse classes. Implemented GetBySlugAsync in repository for fetching movies by slug.

--- Movies.Application/Models/Movie.cs
+++ Movies.Application/Models/Movie.cs
@@ -1,10 +1,24 @@
-﻿namespace Movies.Application.Models
+﻿using System.Text.RegularExpressions;
+
+namespace Movies.Application.Models
 {
-    public class Movie
+    public partial class Movie
     {
         public required Guid Id { get; init; }
         public required string Title { get; set; }
+        public string Slug => GenerateSlug();
         public required int YearOfRelease { get; set; }
         public required IEnumerable<string> Genres { get; init; } = [];
+
+        private string GenerateSlug()
+        {
+            var sluggedTitle = SlugSanitizer().Replace(Title, string.Empty)
+                .ToLowerInvariant().Replace(" ", "-");
+
+            return $"{sluggedTitle}-{YearOfRelease}";
+        }
+
+        [GeneratedRegex("[^0-9A-Za-z _-]", RegexOptions.NonBacktracking, 5)]
+        private static partial Regex SlugSanitizer();
     }
 }
